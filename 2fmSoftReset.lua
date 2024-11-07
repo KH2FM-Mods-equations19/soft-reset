453534a0e3b42205fd5f40eb2ff8d4ae3921de58
equations19 <paulblessing@gmail.com>
2024-11-06 18:06:50 -0600

    3.0.0
    - Update to use KH2 Lua Library (adds support for game version 1.0.0.10)

--- 2fmSoftReset.lua
+++ 2fmSoftReset.lua
@@ -1,41 +1,34 @@
 function _OnInit()
-	canExecute = false
-end
+    kh2libstatus, kh2lib = pcall(require, "kh2lib")
+    if not kh2libstatus then
+        print("ERROR (Soft Reset): KH2-Lua-Library mod is not installed")
+        CanExecute = false
+        return
+    end
 
-function CheckVersion()
-	if GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then
-		if ReadString(0x09A92F0,4) == 'KH2J' then --EGS
-			ConsolePrint('Epic Version Detected')
-			input = 0x29FAD30
-			softreset = 0xABA69A
-			rng = 0x753350
-			spawns = 0x2AE57B8
-			canExecute = true
-		elseif ReadString(0x09A9830,4) == 'KH2J' or  ReadString(0x09A8830,4) == 'KH2J' then --Steam
-			if ReadString(0x09A9830,4) == 'KH2J' then -- Global
-				ConsolePrint('Steam Global Version Detected')
-				offset = 0x0
-			else -- JP
-				ConsolePrint('Steam JP Version Detected')
-				offset = 0x1000
-			end
-			input = 0xBF3120 - offset
-			softreset = 0xABABDA - offset
-			rng = 0x7535C0 - offset
-			spawns = 0x2AE5CF8 - offset
-			canExecute = true
-		end
-	end
+    Log("KH2 Soft Reset 3.0.0")
+    RequireKH2LibraryVersion(1)
+    RequirePCGameVersion()
+
+    CanExecute = kh2lib.CanExecute
+    if not CanExecute then
+        return
+    end
+
+    Input = kh2lib.Input
+    SoftReset = kh2lib.SoftReset
+    RNG = kh2lib.RNG
+    Spawns = kh2lib.Spawns
 end
 
 function _OnFrame()
-	if not canExecute then
-		CheckVersion()
-	end
+    if not CanExecute then
+        return
+    end
 
-	if canExecute and ReadInt(input) == 247042 then
-		WriteByte(softreset, 1)
-		WriteInt(rng, 0x00000001)
-		WriteByte(spawns, 0)
-	end
+    if ReadInt(Input) == 247042 then
+        WriteByte(SoftReset, 1)
+        WriteInt(RNG, 0x00000001)
+        WriteByte(Spawns, 0)
+    end
 end
--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # soft-reset
 
-OpenKH port of Denhonator's KH2 soft reset script. Ported with permission.
+> [!IMPORTANT]
+> In order to support as many game versions as possible, this mod requires that `KH2FM-Mods-equations19/KH2-Lua-Library`
+> is also installed into OpenKH Mods Manager and is enabled.
 
-https://github.com/Denhonator/KHPCSpeedrunTools
+OpenKH port of [Denhonator](https://github.com/Denhonator/KHPCSpeedrunTools)'s KH2 soft reset script. Ported with
+permission.
 
-Icon by Televo.
+Icon from the [Kingdom Hearts Re:Collection](https://github.com/Televo/kingdom-hearts-recollection) by Televo.
 
-https://github.com/Televo/kingdom-hearts-recollection
+## Instructions
+
+In-game, press L1 + L2 + R1 + R2 + Start (or equivalent on your input device).
 
 # Changelog
+
+## 3.0.0
+
+- Update to use KH2 Lua Library (adds support for game version 1.0.0.10)
 
 ## 2.0.0
 
